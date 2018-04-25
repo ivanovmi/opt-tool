76de9c2c04d4516a6c11a4a0821073cfb0e582b4
Mikhail Ivanov <mivanov@mirantis.com>
2018-04-26 01:45:21 +0400

    Change dir generation to recursive method

--- opt_tool/utils.py
+++ opt_tool/utils.py
@@ -10,11 +10,34 @@ def make_directory(dir_name):
             raise
 
 
+def generate_entity():
+    pass
+
+
+def generate_packages(pkg_name, dir_name=''):
+    if isinstance(pkg_name, list):
+        for el in pkg_name:
+            generate_packages(pkg_name[pkg_name.index(el)], dir_name)
+    else:
+        try:
+            for pkg in pkg_name:
+                _pkg = None
+                if isinstance(pkg, dict):
+                    _pkg, pkg = pkg, list(pkg.keys())[0]
+                if pkg != 'entity':
+                    dir_n = dir_name + '/' + str(pkg)
+                    print(dir_n)
+                    make_directory(dir_n)
+                    generate_packages(pkg_name[pkg], dir_n)
+                else:
+                    generate_entity()
+        except TypeError:
+            pass
+
+
 def generate_directories(config):
     for package in config.packages:
-        print(package['name'])
         _dir = str(config.output_dir) + str(package['name'])
         make_directory(_dir)
         for component in package['components']:
-            print(' ' * 2 + list(component.keys())[0])
-            [make_directory(_dir+'/'+str(i)) for i in component.keys()]
+            generate_packages(component, _dir)
